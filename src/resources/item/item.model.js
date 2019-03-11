7c7912959045c0f678cd1d2c4c222e38ef2ed70e
Eric Robinson <soyecr+dr@gmail.com>
2019-03-11 11:12:34 -0500

    Lesson 3 Solution

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,33 @@
 import mongoose from 'mongoose'
+var ObjectId = mongoose.Schema.Types.ObjectId
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50
+    },
+    status: {
+      type: String,
+      required: true,
+      default: 'active',
+      enum: ['active', 'complete', 'pastdue']
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      type: ObjectId,
+      ref: 'user',
+      required: true
+    },
+    list: {
+      type: ObjectId,
+      ref: 'list',
+      required: true
+    }
+  },
+  { timestamps: true }
+)
 export const Item = mongoose.model('item', itemSchema)
