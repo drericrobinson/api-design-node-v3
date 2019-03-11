bc6e7501ed5d7415374882e32d286cd509077bf2
Eric Robinson <soyecr+dr@gmail.com>
2019-03-11 10:00:27 -0500

    Lesson 2 Solution

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,27 @@ import { Router } from 'express'
 
 const router = Router()
 
+const defaultResponse = { message: 'default' }
+
+router
+  .route('/')
+  .get((req, res) => {
+    res.send(defaultResponse)
+  })
+  .post((req, res) => {
+    res.send(defaultResponse)
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    res.send(defaultResponse)
+  })
+  .put((req, res) => {
+    res.send(defaultResponse)
+  })
+  .delete((req, res) => {
+    res.send(defaultResponse)
+  })
+
 export default router
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import config from './config'
 import cors from 'cors'
 import { connect } from './utils/db'
+import itemRouter from './resources/item/item.router'
 
 export const app = express()
 
@@ -14,9 +15,11 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
+app.use('/api/item', itemRouter)
+
 export const start = async () => {
   try {
-    await connect()
+    // await connect()
     app.listen(config.port, () => {
       console.log(`REST API on http://localhost:${config.port}/api`)
     })
